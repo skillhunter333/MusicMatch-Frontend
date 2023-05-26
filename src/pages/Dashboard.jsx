3aef6b34b2f125a92e081a6c3670a7cf03ca3236
skillhunter333 <simon.risspy@gmx.de>
2023-05-26 12:57:15 +0200

    dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,6 +7,23 @@ const Dashboard = () => {
         <h3 className='text-lg font-thin mb-12'>
         People nearby (maps) | Matching | Chats | Favourites (bookmarked people)
         </h3>
+        <div className="flex flex-col items-center justify-center">
+      <h3 className="text-3xl font-bold mb-4">Headline</h3>
+      <div className="grid grid-cols-4 gap-4">
+        <button className="flex items-center justify-center h-32 w-64 bg-blue-500 text-white text-xl font-bold rounded-lg shadow-lg">
+          Button 1
+        </button>
+        <button className="flex items-center justify-center h-32 w-64 bg-green-500 text-white text-xl font-bold rounded-lg shadow-lg">
+          Button 2
+        </button>
+        <button className="flex items-center justify-center h-32 w-64 bg-red-500 text-white text-xl font-bold rounded-lg shadow-lg">
+          Button 3
+        </button>
+        <button className="flex items-center justify-center h-32 w-64 bg-yellow-500 text-white text-xl font-bold rounded-lg shadow-lg">
+          Button 4
+        </button>
+      </div>
+    </div>
       </div>
     </main>
   );

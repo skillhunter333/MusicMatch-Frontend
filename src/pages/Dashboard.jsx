afe2363137682782e93a199993e248e9f8cd9c3a
vitali733 <vitalis.weiss@gmail.com>
2023-05-28 20:37:16 +0200

    make buttons in dashboard wrap

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,40 +5,51 @@ import { useAuthContext } from "../context/AuthContext";
 
 
 const Dashboard = () => {
-  const navigate = useNavigate();
+const navigate = useNavigate();
 
-  const { user } = useAuthContext();
+const { user } = useAuthContext();
 
   return (
-    <main className='w-full flex flex-col items-center mt-[-1px] dark:bg-slate-700 h-fit'>
-      <div className='w-fit mt-28 p-4 flex flex-col items-center'>
-        <h1 className='text-3xl font-semibold mb-8'>Dashboard</h1>
-        <h3 className='text-lg font-thin mb-12'>
-        Chats | People nearby (maps) | Matching | Favourites (bookmarked people)
-        </h3>
-        <div className="flex flex-col items-center justify-center">
+    <>
+      <div className='p-1 flex flex-col items-center'>
+              <h1 className='text-3xl font-semibold mb-8'>Dashboard</h1>
+              <h2 className='text-lg font-thin mb-12'>
+              Chats | People nearby (maps) | Matching | Favourites (bookmarked people)
+              </h2>
+      </div>
 
-      <div className="flex space-x-4">
-        <button onClick={() => navigate('/auth/chat')} className="w-64 flex items-center justify-center flex-1 h-32 bg-zinc-500 text-white text-4xl rounded-lg shadow-lg">
-          <HiChatAlt2 />
-        </button>
-        <button onClick={() => navigate('/auth/map')} className="w-64 flex items-center justify-center flex-1 h-32 bg-zinc-500 text-white text-4xl rounded-lg shadow-lg">
-          <HiMap />
-        </button>
-        <button className=" w-64 flex items-center justify-center flex-1 h-32 bg-zinc-500 text-white text-4xl rounded-lg shadow-lg">
-          <HiOutlineStar />
-        </button>
-        <button className="w-64 flex items-center justify-center flex-1 h-32 bg-zinc-500 text-white text-4xl rounded-lg shadow-lg">
-          <HiSparkles />
-        </button>
-        <button onClick={() => navigate(`/auth/profile/${user._id}`)} className="w-64 flex items-center justify-center flex-1 h-32 bg-zinc-500 text-white text-4xl rounded-lg shadow-lg">
-          <HiUserCircle />
-        </button>
-      </div>
-    </div>
-      </div>
-    </main>
+
+              <div className="flex flex-wrap space-y-4 space-x-4 ">
+                    <div>
+                      <button onClick={() => navigate('/auth/chat')} className="w-64 flex items-center justify-center  h-32 bg-zinc-500 text-white text-4xl rounded-lg shadow-lg">
+                        <HiChatAlt2 />
+                      </button>
+                    </div>
+                    <div>
+                      <button onClick={() => navigate('/auth/map')} className="w-64 flex items-center justify-center  h-32 bg-zinc-500 text-white text-4xl rounded-lg shadow-lg">
+                        <HiMap />
+                      </button>
+                    </div>
+                    <div>
+                      <button className=" w-64 flex items-center justify-center  h-32 bg-zinc-500 text-white text-4xl rounded-lg shadow-lg">
+                        <HiOutlineStar />
+                      </button>
+                    </div>
+                      <div>
+                      <button className="w-64 flex items-center justify-center h-32 bg-zinc-500 text-white text-4xl rounded-lg shadow-lg">
+                        <HiSparkles />
+                      </button>
+                    </div>
+                    <div>
+                      <button onClick={() => navigate(`/auth/profile/${user._id}`)} className="w-64 flex items-center justify-center flex-1 h-32 bg-zinc-500 text-white text-4xl rounded-lg shadow-lg">
+                        <HiUserCircle />
+                      </button>
+                    </div>  
+              </div>
+      </>
   );
 };
 
 export default Dashboard;
+
+

395809b85663fdfddd8863225f8eaa5c898c1769
Simon R <122458621+skillhunter333@users.noreply.github.com>
2023-06-06 11:26:09 +0200

    update profile pic via cloudinary upload

---
+++ src/pages/CloudinaryUploadWidget.jsx
@@ -0,0 +1,102 @@
+// import React, { Component } from "react";
+
+// class CloudinaryUploadWidget extends Component {
+//   componentDidMount() {
+//     const cloudName = "dz6opiy3z";
+//     const uploadPreset = "ufldzebl";
+
+//     //   https://cloudinary.com/documentation/upload_widget_reference
+
+//     let myWidget = window.cloudinary.createUploadWidget(
+//       {
+//         cloudName: cloudName,
+//         uploadPreset: uploadPreset,
+//         cropping: true,
+//         showSkipCropButton: false,
+//         croppingAspectRatio: 1,
+//         croppingDefaultSelectionRatio: 1,
+//         croppingValidateDimensions: true,
+//         sources: ["local", "camera", "facebook", "instagram"], // restrict the upload sources to URL and local files
+//         multiple: false, //restrict upload to a single file
+//         showCompletedButton: true,
+//         singleUploadAutoClose: true,
+
+//         clientAllowedFormats: ["images"],
+
+//         maxImageWidth: 720, //Scales the image down to a width of    pixels before uploading
+//       },
+//       (error, result) => {
+//         if (!error && result && result.event === "success") {
+//           console.log("Done! Here is the image info: ", result.info);
+//           console.log(result.info.secure_url);
+//         }
+//       }
+//     );
+//     document.getElementById("upload_widget").addEventListener(
+//       "click",
+//       function () {
+//         myWidget.open();
+//       },
+//       false
+//     );
+//   }
+
+//   render() {
+//     return (
+//       // <button id="upload_widget" className="cloudinary-button">
+//       //   Upload
+//       // </button>
+//       <></>
+//     );
+//   }
+// }
+
+// export default CloudinaryUploadWidget;
+
+import React, { useEffect } from "react";
+
+const CloudinaryUploadWidget = ({ onUploadSuccess }) => {
+  useEffect(() => {
+    const cloudName = "dz6opiy3z";
+    const uploadPreset = "ufldzebl";
+
+    let myWidget = window.cloudinary.createUploadWidget(
+      {
+        cloudName: cloudName,
+        uploadPreset: uploadPreset,
+        cropping: true,
+        showSkipCropButton: false,
+        croppingAspectRatio: 1,
+        croppingDefaultSelectionRatio: 1,
+        croppingValidateDimensions: true,
+        sources: ["local", "camera", "facebook", "instagram"],
+        multiple: false,
+        showCompletedButton: true,
+        singleUploadAutoClose: true,
+        // clientAllowedFormats: ["images"],
+        maxImageWidth: 720,
+      },
+      (error, result) => {
+        if (!error && result && result.event === "success") {
+          console.log("Done! Here is the image info: ", result.info);
+          console.log(result.info.secure_url);
+
+          // Pass the secure URL to the callback function
+          onUploadSuccess(result.info.secure_url);
+        }
+      }
+    );
+
+    document.getElementById("upload_widget").addEventListener(
+      "click",
+      function () {
+        myWidget.open();
+      },
+      false
+    );
+  }, [onUploadSuccess]);
+
+  return <></>;
+};
+
+export default CloudinaryUploadWidget;
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,10 +1,11 @@
+import React, { useState, useEffect } from "react";
 import { useAuthContext } from "../context/AuthContext";
 import { useParams } from "react-router-dom";
-import { useState, useEffect, useContext } from "react";
 import getUserById from "../utils.js/getUserById";
 import axios from "axios";
 import { Modal, Button, TextInput } from "flowbite-react";
 import { Link } from "react-router-dom";
+import CloudinaryUploadWidget from "./CloudinaryUploadWidget";
 
 //Keine Self-Ratings in Datenbank? + evtl. Minitext zu Skills/Interests
 
@@ -13,7 +14,7 @@ const ProfilePage = () => {
   //user is based on params id
   const [user, setUser] = useState(null);
   const [openModal, setOpenModal] = useState(0);
-  const [isOpen, setIsOpen] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
   const [charDesc, setCharDesc] = useState("");
   const [charType, setCharType] = useState("");
   const [charName, setCharName] = useState("");
@@ -161,20 +162,20 @@ const ProfilePage = () => {
     }
   }
 
-  async function handleUpdateProfilePic(imgUrl) {
+  const handleUploadSuccess = async (secureUrl) => {
     try {
+      console.log(`this is the new imgUrl from the upload ${secureUrl}`);
       const { data } = await axios.put(
         `${import.meta.env.VITE_API_URL}/users/me`,
-        { imgUrl: imgUrl },
+        { imgUrl: secureUrl },
         { withCredentials: true }
       );
-      console.log(imgUrl);
       updateUser();
     } catch (error) {
       if (error.response.status !== 400) toastError(error.message);
       console.log(error);
     }
-  }
+  };
 
   function handleCloseModal() {
     try {
@@ -184,146 +185,154 @@ const ProfilePage = () => {
       console.log(error);
     }
   }
-  function handleCloseModalProfilePic() {
-    try {
-      setIsOpen(false);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  // function handleCloseModalProfilePic() {
+  //   try {
+  //     setIsOpen(false);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 
   //
   return (
     //main container
     <div className="bg-slate-50">
-        <div className="flex  gap-4 flex-col pt-20 ml-8 mr-8">
-          {/*ROW(1) row with img, name and description*/}
-          <div className="flex  h-48   ">
-            {/*COL column with image*/}
+      <div className="flex  gap-4 flex-col pt-20 ml-8 mr-8">
+        {/*ROW(1) row with img, name and description*/}
+        <div className="flex  h-48   ">
+          {/*COL column with image*/}
 
-            <img
-              className="w-48 rounded-full"
-              // className="rounded-full"
-              src={user && user.imgUrl}
-              alt="Profile Picture"
-              onClick={() => {
-                setIsOpen(true);
-              }}
-            />
+          <img
+            className="w-48 rounded-full"
+            // className="rounded-full"
+            src={user && user.imgUrl}
+            alt="Profile Picture"
+            id="upload_widget"
+            // onClick={() => {
+            //   setIsOpen(true);
+            // }}
+          />
 
-            {/*COL column with name and description*/}
-            <div className="flex  flex-col w-11/12 ml-4 gap-4 p-2  ">
-              {/*ROW with first and lastname*/}
-              {user && (
-                <div className="flex  gap-4">
-                  <h2 class="text-4xl  dark:text-white">
-                    {user.firstName ? user.firstName : "Vorname"}
-                  </h2>
-                  <h2 class="text-4xl  dark:text-white">
-                    {user.lastName ? user.lastName : "Nachname"}
-                  </h2>
-                </div>
-              )}
-              <div className="w-11/12  ">
-                <p>{user && user.userDescription}</p>
+          {/*COL column with name and description*/}
+          <div className="flex  flex-col w-11/12 ml-4 gap-4 p-2  ">
+            {/*ROW with first and lastname*/}
+            {user && (
+              <div className="flex  gap-4">
+                <h2 class="text-4xl  dark:text-white">
+                  {user.firstName ? user.firstName : "Vorname"}
+                </h2>
+                <h2 class="text-4xl  dark:text-white">
+                  {user.lastName ? user.lastName : "Nachname"}
+                </h2>
               </div>
+            )}
+            <div className="w-11/12  ">
+              <p>{user && user.userDescription}</p>
             </div>
           </div>
-
-          {/* ROW with interests */}
-
-          <div className="flex flex-col gap-2 bg-slate-200 p-4 rounded ">
-            <h2 className="font-bold text-mmGrey">Interessen</h2>
-            {/* ROW  for single interests*/}
-            {user &&
-              user.interests.map((interest, index) => (
-                <div key={index} className="bg-slate-100 rounded">
-                  <div className="flex flex-wrap p-2 ">
-                    <Link
-                      to="/auth/skills"
-                      onClick={() => {}}
-                      className="bg-yellow-100 text-yellow-800 text-l font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300 hover:bg-green-200 hover:text-green-900"
-                    >
-                      {interest.name}
-                    </Link>
-                    <button
-                      charType="interest"
-                      onClick={handleDescClick}
-                      className={`pl-2 mt-2 text-left ${
-                        interest.description === "" ? "text-slate-400" : ""
-                      } `}
-                    >
-                      {interest.description === ""
-                        ? "Beschreibung einfügen..."
-                        : interest.description}
-                    </button>
-                  </div>
-                </div>
-              ))}
-          </div>
-
-          {/* ROW with skills */}
-          <div className="flex flex-col gap-2 bg-slate-200 p-4 mb-8 rounded">
-            <h2 className="font-bold text-mmGrey">Fähigkeiten</h2>
-            {/* ROW  for single interests*/}
-            {user &&
-              user.skills.map((skill, index) => (
-                <div key={index} className="bg-slate-100 rounded">
-                  <div className="flex flex-wrap p-2 ">
-                    <Link
-                      to="/auth/skills"
-                      onClick={handleInterestClick}
-                      className="bg-yellow-100 text-yellow-800 text-l font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300 hover:bg-green-200 hover:text-green-900"
-                    >
-                      {skill.name}
-                    </Link>
-                    <button
-                      charType="skill"
-                      onClick={handleDescClick}
-                      className={`pl-2 mt-2 text-left ${
-                        skill.description === "" ? "text-slate-400" : ""
-                      } `}
-                    >
-                      {skill.description === ""
-                        ? "Beschreibung einfügen..."
-                        : skill.description}
-                    </button>
-                  </div>
-                </div>
-              ))}
-          </div>
-
-          <Modal onClose={handleCloseModal} show={openModal ? true : false}>
-            <Modal.Header>Beschreibung anpassen</Modal.Header>
-            <Modal.Body>
-              <div className="space-y-6">
-                <TextInput
-                  defaultValue={charDesc}
-                  placeholder="beschreibe dein Interesse/Skill"
-                />
-              </div>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button onClick={handleSafeDesc}>Änderung speichern</Button>
-              <Button color="gray" onClick={handleCloseModal}>
-                <p>Abbrechen</p>
-              </Button>
-            </Modal.Footer>
-
-            {/* Profile Picture edit modal: */}
-          </Modal>
-          <Modal onClose={handleCloseModalProfilePic} show={isOpen ? true : false}>
-            <Modal.Header>Profilbild bearbeiten</Modal.Header>
-            <Modal.Body>
-              <div className="space-y-6"></div>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button color="gray" onClick={handleCloseModalProfilePic}>
-                <p>Abbrechen</p>
-              </Button>
-            </Modal.Footer>
-          </Modal>
         </div>
+
+        <div>
+          <CloudinaryUploadWidget onUploadSuccess={handleUploadSuccess} />
+        </div>
+
+        {/* ROW with interests */}
+
+        <div className="flex flex-col gap-2 bg-slate-200 p-4 rounded ">
+          <h2 className="font-bold text-mmGrey">Interessen</h2>
+          {/* ROW  for single interests*/}
+          {user &&
+            user.interests.map((interest, index) => (
+              <div key={index} className="bg-slate-100 rounded">
+                <div className="flex flex-wrap p-2 ">
+                  <Link
+                    to="/auth/skills"
+                    onClick={() => {}}
+                    className="bg-yellow-100 text-yellow-800 text-l font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300 hover:bg-green-200 hover:text-green-900"
+                  >
+                    {interest.name}
+                  </Link>
+                  <button
+                    charType="interest"
+                    onClick={handleDescClick}
+                    className={`pl-2 mt-2 text-left ${
+                      interest.description === "" ? "text-slate-400" : ""
+                    } `}
+                  >
+                    {interest.description === ""
+                      ? "Beschreibung einfügen..."
+                      : interest.description}
+                  </button>
+                </div>
+              </div>
+            ))}
+        </div>
+
+        {/* ROW with skills */}
+        <div className="flex flex-col gap-2 bg-slate-200 p-4 mb-8 rounded">
+          <h2 className="font-bold text-mmGrey">Fähigkeiten</h2>
+          {/* ROW  for single interests*/}
+          {user &&
+            user.skills.map((skill, index) => (
+              <div key={index} className="bg-slate-100 rounded">
+                <div className="flex flex-wrap p-2 ">
+                  <Link
+                    to="/auth/skills"
+                    onClick={handleInterestClick}
+                    className="bg-yellow-100 text-yellow-800 text-l font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300 hover:bg-green-200 hover:text-green-900"
+                  >
+                    {skill.name}
+                  </Link>
+                  <button
+                    charType="skill"
+                    onClick={handleDescClick}
+                    className={`pl-2 mt-2 text-left ${
+                      skill.description === "" ? "text-slate-400" : ""
+                    } `}
+                  >
+                    {skill.description === ""
+                      ? "Beschreibung einfügen..."
+                      : skill.description}
+                  </button>
+                </div>
+              </div>
+            ))}
+        </div>
+
+        <Modal onClose={handleCloseModal} show={openModal ? true : false}>
+          <Modal.Header>Beschreibung anpassen</Modal.Header>
+          <Modal.Body>
+            <div className="space-y-6">
+              <TextInput
+                defaultValue={charDesc}
+                placeholder="beschreibe dein Interesse/Skill"
+              />
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={handleSafeDesc}>Änderung speichern</Button>
+            <Button color="gray" onClick={handleCloseModal}>
+              <p>Abbrechen</p>
+            </Button>
+          </Modal.Footer>
+
+          {/* Profile Picture edit modal: */}
+        </Modal>
+        {/* <Modal
+          onClose={handleCloseModalProfilePic}
+          show={isOpen ? true : false}
+        >
+          <Modal.Header>Profilbild bearbeiten</Modal.Header>
+          <Modal.Body>
+            <div className="space-y-6"></div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button color="gray" onClick={handleCloseModalProfilePic}>
+              <p>Abbrechen</p>
+            </Button>
+          </Modal.Footer>
+        </Modal> */}
+      </div>
     </div>
   );
 };

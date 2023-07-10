d562b005582f4285965fc29473ddd1979ee9d841
vitali733 <vitalis.weiss@gmail.com>
2023-07-10 11:42:27 +0200

    removed dotenv file from tracking

--- src/pages/SetupProfile.jsx
+++ src/pages/SetupProfile.jsx
@@ -1,59 +1,73 @@
 import { Button } from "flowbite-react";
 import { useAuthContext } from "../context/AuthContext";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+import { AddressAutofill } from "@mapbox/search-js-react";
+import ReactDOM from "react-dom";
 
 const SetupProfile = () => {
   const { user } = useAuthContext();
 
-  const [{ firstName, lastName, address, postalCode, radius }, setForm] =
-    useState({
-      firstName: "",
-      lastName: "",
-      address: "",
-      postalCode: "",
-      radius: ""
-    });
+  const [
+    { firstName, lastName, street, city, country, postalCode, radius },
+    setForm,
+  ] = useState({
+    firstName: "",
+    lastName: "",
+    street: "",
+    city: "",
+    country: "",
+    postalCode: "",
+    radius: "",
+  });
 
-  useEffect(()=>{
+  useEffect(() => {
     setForm({
       firstName: user.firstName && user.firstName,
       lastName: user.lastName && user.lastName,
-      address: user.address && user.address,
+      street: user.address.street && user.address.street,
+      city: user.address.city && user.address.city,
+      country: user.address.country && user.address.country,
       postalCode: user.postalCode && user.postalCode,
-      radius: user.settings.radius && user.settings.radius
-    })
-  },[])
+      radius: user.settings.radius && user.settings.radius / 1000,
+    });
+  }, []);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
+    console.log(`${name}:  (${value})`);
     setForm((prev) => ({ ...prev, [name]: value }));
   };
 
-  async function handleSubmit(event) {
+  async function handleSubmit() {
     try {
       console.log(
-        `firstName: (${firstName}) lastName: (${lastName}) address: (${address}) postalCode: (${postalCode}) radius: (${radius})`
+        `firstName: (${firstName}) lastName: (${lastName}) street: (${street}) city: (${city}) country: (${country}) postalCode: (${postalCode}) radius: (${radius})`
       );
       const { data } = await axios.put(
         `${import.meta.env.VITE_API_URL}/users/me/`,
         {
           firstName,
           lastName,
-          address,
+          address: {
+            street: street,
+            city: city,
+            country: country,
+          },
           postalCode,
           settings: {
-            radius: radius*1000
-          } 
+            radius: radius * 1000,
+          },
         },
         {
           withCredentials: true,
         }
       );
       console.log(data);
+      console.log("blub");
     } catch (error) {
-      if (error.response.status !== 400) toastError(error.message);
+      // if (error.response.status !== 400) toastError(error.message);
       console.log(error);
     }
   }
@@ -65,17 +79,14 @@ const SetupProfile = () => {
 
         <div className="flex flex-col gap-2">
           <div className="flex gap-2">
-       
             <input
               className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               type="text"
               placeholder="Vorname"
-              name="fistName"
+              name="firstName"
               defaultValue={user && user.firstName}
               onChange={handleChange}
             ></input>
-
-
 
             <input
               className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
@@ -96,23 +107,61 @@ const SetupProfile = () => {
             onChange={handleChange}
           ></input>
 
-          <input
-            className="mt-4 appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            type="text"
-            placeholder="Adresse"
-            name="adress"
-            defaultValue={user && user.adress}
-            onChange={handleChange}
-          ></input>
+          {/* <input
+              className="mt-4 appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              type="text"
+              placeholder="Adresse"
+              name="adress"
+              defaultValue={user && user.adress}
+              onChange={handleChange}
+            ></input> */}
 
-          <input
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            type="text"
-            placeholder="Postleitzahl"
-            name="postalCode"
-            defaultValue={user && user.postalCode}
-            onChange={handleChange}
-          ></input>
+          <form>
+            <AddressAutofill accessToken={`${import.meta.env.VITE_MAPBOX_KEY}`}>
+              <input
+                autoComplete="street-address"
+                type="text"
+                placeholder="Straße, Hausnummer"
+                name="street"
+                defaultValue={user && user.address.street}
+                onChange={handleChange}
+              />
+
+              {/* <input
+                autoComplete="address-line1"
+                type="text"
+                placeholder="Straße, Hausnummer"
+                name="street"
+                defaultValue={user.adress && user.address.street}
+                onChange={handleChange}
+              /> */}
+
+              <input
+                name="city"
+                placeholder="Stadt"
+                type="text"
+                autoComplete="address-level2"
+                defaultValue={user && user.address.city}
+                onChange={handleChange}
+              />
+              <input
+                name="country"
+                placeholder="Land"
+                type="text"
+                autoComplete="country"
+                defaultValue={user && user.address.country}
+                onChange={handleChange}
+              />
+              <input
+                name="postalCode"
+                placeholder="Postleitzahl"
+                type="text"
+                autoComplete="postal-code"
+                defaultValue={user && user.postalCode}
+                onChange={handleChange}
+              />
+            </AddressAutofill>
+          </form>
 
           <p className="mt-4 pl-1 block uppercase tracking-wide text-gray-700 text-xs font-bold">
             Suchradius (km)
@@ -122,7 +171,7 @@ const SetupProfile = () => {
             type="text"
             placeholder="Suchradius in km"
             name="radius"
-            defaultValue={user && user.settings.radius / 1000}
+            defaultValue={user && user.settings.radius}
             onChange={handleChange}
           ></input>
         </div>
